4d59933b34378f2ce8372fa3f7c7ba0d0223af1b
Kubilay Bozak <48950686+kubilaybzk@users.noreply.github.com>
2023-04-22 18:55:39 +0300

    LazyLoading

---
+++ Repository/Contracts/IBookRepository.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Contracts
+{
+    public interface IBookRepository :IRepositoryBase<Book>
+    {
+        void CreateOneBook(Book book);
+        void DeleteOneBook(Book book);
+        void UpdateOneBook(Book book);
+
+        IQueryable<Book> GetAllBooks(bool trackChanges);
+
+        IQueryable<Book> GetOneBookById(bool trackChanges,int id);
+
+
+    }
+}
---
+++ Repository/EFCore/BookRepository.cs
@@ -0,0 +1,28 @@
+﻿using Entities.Models;
+using Repository.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.EFCore
+{
+    public class BookRepository : RepositoryBase<Book>, IBookRepository
+    {
+        public BookRepository(RepositoryContext context) : base(context)
+        {
+
+        }
+
+        public void CreateOneBook(Book book) => Update(book);
+
+        public void DeleteOneBook(Book book) => Delete(book);
+
+        public IQueryable<Book> GetAllBooks(bool trackChanges)=> FindAll(trackChanges).OrderBy(b=>b.Title);
+
+        public IQueryable<Book> GetOneBookById(bool trackChanges, int id) => FindByCondition(trackChanges, b => b.Id.Equals(id));
+
+        public void UpdateOneBook(Book book) => Update(book);
+    }
+}
