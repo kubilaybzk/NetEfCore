113d2c203aa10af6d6a3b6775d51e17e743c1d99
Kubilay Bozak <48950686+kubilaybzk@users.noreply.github.com>
2023-04-22 22:52:41 +0300

    ServicesExtensions  ile Programcs düzenlendi.

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Repository.EFCore;
+using WebApi.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,9 +10,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-builder.Services.AddDbContext<RepositoryContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("MyDatabase")));
+builder.Services.ConfigureSqlContext(builder.Configuration);
 
 var app = builder.Build();
 
